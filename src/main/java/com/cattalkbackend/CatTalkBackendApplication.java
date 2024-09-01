260fa24e0f325d8ca5c9056a472cb56ff688dd02
강수림 <eathergs012@gmail.com>
2024-09-01 20:05:01 +0900

    [2024_09_02] first commit

---
+++ src/main/java/com/cattalkbackend/CatTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cattalkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatTalkBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CatTalkBackendApplication.class, args);
+  }
+
+}
